554c453d89b285db7786a2660cf822100248bd42
Gerasimos Gakis <contact@gerasimosgakis.com>
2018-02-22 19:08:48 +0000

    feat: Add service worker and cache pages when visiting as well as deleting cache when not needed

--- js/indexController.js
+++ js/indexController.js
@@ -1,23 +1,12 @@
-/*if (!navigator.serviceWorker) {
-  return;
-}
-navigator.serviceWorker.register('/sw.js').then(function() {
-  console.log('Servicer worker registered');
-}).catch(function() {
-  console.log('Service worker failed to register');
-});*/
-
 (function() {
   'use strict';
 
-  // TODO - 2: Register the service worker
+  // Register the service worker
   if (!navigator.serviceWorker) {
     console.log('Service worker not supported');
     return;
   }
-  navigator.serviceWorker.register('service-worker.js', {
-    scope: 'below/'
-  })
+  navigator.serviceWorker.register('service-worker.js')
   .then(function(registration) {
     console.log('Registered at scope:', registration.scope);
   })
