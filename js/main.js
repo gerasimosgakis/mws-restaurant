a6f0e6c30a709656779630fa1eff6d5ef3c23dab
Gerasimos Gakis <contact@gerasimosgakis.com>
2018-07-28 01:02:21 +0100

    need to clean up the code

--- js/main.js
+++ js/main.js
@@ -284,3 +284,10 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
+
+const swap_map = () => {    
+  if (document.getElementById('map').style.display === 'none'){        
+    document.getElementById('map').style.display = 'block';
+    document.getElementById('static_map').style.display = 'none';
+  }    
+}
--- js/swRegister.js
+++ js/swRegister.js
@@ -3,14 +3,7 @@
 
 // Register the service worker
 if (navigator.serviceWorker) {
-  
-  navigator.serviceWorker.register('service-worker.js')
-  .then(function(registration) {
-    console.log('Registered at scope:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Registration failed:', error);
-  });
+  navigator.serviceWorker.register('service-worker.js');
 }
   // navigator.serviceWorker.ready.then(swRegistration => {
   //   return swRegistration.sync.register('myFirstSync');
